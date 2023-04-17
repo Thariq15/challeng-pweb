3516459ff191648c9c44e98504f937f2fa9facd3
Thariq15 <thariqaziz1501@gmail.com>
2023-04-17 08:57:24 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const unReadMessage = document.querySelectorAll(".unread");
+const unread = document.getElementById("notifications");
+const markAll = document.getElementById("mark_all");
+
+unread.innerText = unReadMessage.length;
